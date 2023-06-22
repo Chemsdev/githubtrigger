3e243c9514f785dbfc141a40471823ee1f6e51ad
Ounissi Chems <chemscreative@gmail.com>
2023-06-22 14:35:30 +0200

    collect.py maj

--- transdev_forecast/__init__.py
+++ transdev_forecast/__init__.py
@@ -2,35 +2,8 @@ import datetime
 import logging
 
 import azure.functions as func
-import mysql.connector
+from . import collect
 
-def db_connect(host: str, user: str, password: str, database: str):
-    db = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    return db
-
-def requete():
-    conn = db_connect(
-        host="chemsdineserver.mysql.database.azure.com",
-        user="chemsdine",
-        password="Ounissi69800",
-        database="bdd_trigger"
-    )
-    cursor = conn.cursor()
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS `trigger`
-        (id INT AUTO_INCREMENT PRIMARY KEY,
-        value TEXT)
-    ''')
-
-    cursor.execute("INSERT INTO `trigger` (value) VALUES ('fonctionne biennnn !')")
-    conn.commit()
-    print("Donnée insérée avec succès !")
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -41,4 +14,7 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
-    requete()
+    collect.requete()
+    
+
+
