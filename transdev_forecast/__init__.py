b7d9b1188490dc190d2397e15c26b98181e63fdf
Ounissi Chems <chemscreative@gmail.com>
2023-06-22 09:38:58 +0200

    test on met les fonctions dans le fichier init.py

--- transdev_forecast/__init__.py
+++ transdev_forecast/__init__.py
@@ -2,7 +2,36 @@ import datetime
 import logging
 
 import azure.functions as func
-from collect import *
+import mysql.connector
+
+def db_connect(host: str, user: str, password: str, database: str):
+    db = mysql.connector.connect(
+        host=host,
+        user=user,
+        password=password,
+        database=database
+    )
+    return db
+
+def requete():
+    conn = db_connect(
+        host="chemsdineserver.mysql.database.azure.com",
+        user="chemsdine",
+        password="Ounissi69800",
+        database="bdd_trigger"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS `trigger`
+        (id INT AUTO_INCREMENT PRIMARY KEY,
+        value TEXT)
+    ''')
+
+    cursor.execute("INSERT INTO `trigger` (value) VALUES ('fonctionne biennnn !')")
+    conn.commit()
+    print("Donnée insérée avec succès !")
+
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
